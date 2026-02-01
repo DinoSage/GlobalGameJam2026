346f5c2c1011f630880172d221ed9f5d7e93f7c2
Ansh Agrawal <anshag@gmail.com>
2026-02-01 13:06:01 -0600

    + Basic Player Horizontal Acceleration & Max Speed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float acceleration;
+    [SerializeField] private float maxHorizontalSpeed;
+    
+    private Rigidbody2D rb;
+    private int horizontalInputScale;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        horizontalInputScale = 0;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontalInputScale -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            horizontalInputScale += 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float force = rb.mass * acceleration;
+        rb.AddForce(Vector2.right * (horizontalInputScale * force), ForceMode2D.Force);
         
+        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed), rb.velocity.y);
     }
 }
