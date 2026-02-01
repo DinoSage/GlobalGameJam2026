6b9404ef09905c33c7c36c2e0fd834a2244c92de
Ansh Agrawal <anshag@gmail.com>
2026-02-01 13:19:58 -0600

    + Player Deceleration

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,34 +7,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float acceleration;
     [SerializeField] private float maxHorizontalSpeed;
     
-    private Rigidbody2D rb;
-    private int horizontalInputScale;
+    private Rigidbody2D _rb;
+    private int _horizontalInputScale;
     
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        horizontalInputScale = 0;
+        _horizontalInputScale = 0;
 
         if (Input.GetKey(KeyCode.A))
         {
-            horizontalInputScale -= 1;
+            _horizontalInputScale -= 1;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            horizontalInputScale += 1;
+            _horizontalInputScale += 1;
         }
     }
 
     void FixedUpdate()
     {
-        float force = rb.mass * acceleration;
-        rb.AddForce(Vector2.right * (horizontalInputScale * force), ForceMode2D.Force);
+        if (_horizontalInputScale != 0)
+        {
+            // Accelerate (horizontally)   
+            float force = _rb.mass * acceleration;
+            _rb.AddForce(Vector2.right * (_horizontalInputScale * force), ForceMode2D.Force);
+        }
+        else
+        {
+            // Decelerate (horizontally)
+            float oppDirection = _rb.velocity.x > 0 ? -1 : 1;
+            _rb.AddForce(Vector2.right * (oppDirection * acceleration), ForceMode2D.Force);
+        }
         
-        rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed), rb.velocity.y);
+        // Clamp horizontal speed
+        _rb.velocity = new Vector2(Mathf.Clamp(_rb.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed), _rb.velocity.y);
     }
 }
