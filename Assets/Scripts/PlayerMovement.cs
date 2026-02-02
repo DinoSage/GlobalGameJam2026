b3ea0847c5097e5ea8b9be232c50cf446abfe9eb
Ansh Agrawal <anshag@gmail.com>
2026-02-01 23:08:07 -0600

    + New Input System AND + Player Acceleration using Lerp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float acceleration;
-    [SerializeField] private float maxHorizontalSpeed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float accelTime; // in seconds
     
     private Rigidbody2D _rb;
     private int _horizontalInputScale;
+    private Coroutine _moveRoutine;
     
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        Debug.Log(("TEST"));
+        //_rb.velocity = new Vector2(5, 0);
     }
 
     void Update()
@@ -30,22 +34,37 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    public void OnMoveX(InputValue inputValue)
     {
-        if (_horizontalInputScale != 0)
+        float valueX = inputValue.Get<float>();
+        
+        // Stop existing routine if exists
+        if (_moveRoutine != null)
         {
-            // Accelerate (horizontally)   
-            float force = _rb.mass * acceleration;
-            _rb.AddForce(Vector2.right * (_horizontalInputScale * force), ForceMode2D.Force);
+            StopCoroutine(_moveRoutine);
+        }
+        
+        if (valueX != 0)
+        {
+            _moveRoutine = StartCoroutine(Accelerate(valueX));
         }
         else
         {
-            // Decelerate (horizontally)
-            float oppDirection = _rb.velocity.x > 0 ? -1 : 1;
-            _rb.AddForce(Vector2.right * (oppDirection * acceleration), ForceMode2D.Force);
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
         }
-        
-        // Clamp horizontal speed
-        _rb.velocity = new Vector2(Mathf.Clamp(_rb.velocity.x, -maxHorizontalSpeed, maxHorizontalSpeed), _rb.velocity.y);
     }
+
+
+    IEnumerator Accelerate(float direction)
+    {
+        float startTime = Time.time;
+        while (Mathf.Abs(_rb.velocity.x) < maxSpeed)
+        {
+            float timeInterpolate = (Time.time -  startTime) / accelTime;
+            float velX = Mathf.Lerp(0, maxSpeed, timeInterpolate);
+            _rb.velocity = new Vector2(direction * velX, _rb.velocity.y);
+            yield return null;
+        }
+    }
+    
 }
